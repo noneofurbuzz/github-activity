b486f83889c693ec80126921ecff79e7f21ff6d6
Ugochi Ike <ugochiike26@gmail.com>
2024-11-11 17:29:02 +0100

    feat: fetch user activity

--- bin/index.js
+++ bin/index.js
@@ -9,13 +9,64 @@ program
 program
     .argument('<github-username>')
     .action((username) => {
-        console.log(`Your username is ${username}`)
         axios.get(`https://api.github.com/users/${username}/events`)
         .then(response => {
-            console.log(response.data)
+            for(let i = 0;i <= response.data.length;i++){
+                if (response.data[i].type == "IssuesEvent"){
+                    console.log(`- ${response.data[i].payload.action} an issue in ${response.data[i].repo.name}`)
+                }
+                else if (response.data[i].type == "CommitCommentEvent"){
+                    console.log(`- commented on a commit in  ${response.data[i].repo.name}`) 
+                    
+                }
+                else if (response.data[i].type == "CreateEvent"){
+                    console.log(`- created a ${response.data[i].payload.ref_type} in the ${response.data[i].payload.master_branch} branch of ${response.data[i].repo.name}`) 
+                }
+                else if (response.data[i].type == "DeleteEvent"){
+                    console.log(`- deleted a ${response.data[i].payload.ref_type} in ${response.data[i].repo.name}`) 
+                }
+                else if (response.data[i].type == "ForkEvent"){
+                    console.log(`- forked ${response.data[i].repo.name}`) 
+                }
+                else if (response.data[i].type == "GollumEvent"){
+                    console.log(`- updated wiki page in ${response.data[i].repo.name}`) 
+                }
+                else if (response.data[i].type == "IssueCommentEvent"){
+                    console.log(`- ${response.data[i].payload.action} a comment on an issue in ${response.data[i].repo.name}`) 
+                    
+                }
+                else if (response.data[i].type == "MemberEvent"){
+                    console.log(`- ${response.data[i].payload.action} a collaborator to ${response.data[i].repo.name}`) 
+                    
+                }
+                else if (response.data[i].type == "PublicEvent"){
+                    console.log(`- made ${response.data[i].repo.name} public`) 
+                }
+                else if (response.data[i].type == "PullRequestEvent"){
+                    console.log(`- ${response.data[i].payload.action} a pull request in ${response.data[i].repo.name}`) 
+                }
+                else if (response.data[i].type == "PullRequestReviewEvent"){
+                    console.log(`- ${response.data[i].payload.action} a pull request review in ${response.data[i].repo.name}`) 
+                }
+                else if (response.data[i].type == "PullRequestReviewCommentEvent"){
+                    console.log(`- commented on a pull request review in ${response.data[i].repo.name}`) 
+                }
+                else if (response.data[i].type == "PullRequestReviewThreadEvent"){
+                    console.log(`- ${response.data[i].payload.action} a comment thread on a pull request in ${response.data[i].repo.name}`) 
+                }
+                else if (response.data[i].type == "PushEvent"){
+                    console.log(`- pushed ${response.data[i].payload.size} ${response.data[i].payload.size > 1 ? "commits":"commit"} to ${response.data[i].repo.name}`) 
+                }
+                else if (response.data[i].type == "ReleaseEvent"){
+                    console.log(`- ${response.data[i].payload.action} a release in ${response.data[i].repo.name}`) 
+                }
+                else if (response.data[i].type == "WatchEvent"){
+                    console.log(`- starred ${response.data[i].repo.name}`) 
+                }
+            }
         })
         .catch(error => {
-            console.error('Error while fetching data')
+            console.error(error)
         })
     })
 program.parse(process.argv)
